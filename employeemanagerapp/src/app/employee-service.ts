b5d97062437f20907072c3131852b8f3f40e1774
AndreRH09 <aanazcoh@unsa.edu.pe>
2026-08-24 01:29:14 -0500

    featadd)> service completed

--- employeemanagerapp/src/app/employee-service.ts
+++ employeemanagerapp/src/app/employee-service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +11,23 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) { }
 
-  private getallEmployee(): Observable<any> {
-    return this.http.get<any>(`${this.apiServerUrl}/employee/all`);
+  private getallEmployee(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  
+  private addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
+
+    
+  private updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+
+  
+  private deleteEmployee(employeeId: number): Observable<Employee> {
+    return this.http.delete<Employee>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
   }
 }
